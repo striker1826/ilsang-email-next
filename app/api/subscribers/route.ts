83793905f8fbca4dc21d96b61ed9b1155b9db6a2
김민섭 <alstjq123579@daum.net>
2025-02-01 22:17:21 +0900

    feat

--- app/api/subscribers/route.ts
+++ app/api/subscribers/route.ts
@@ -1,5 +1,3 @@
-import nodemailer from "nodemailer";
-
 export async function POST(request: Request) {
   const { email } = await request.json();
 
@@ -15,11 +13,9 @@ export async function POST(request: Request) {
     }
   );
 
-  const isExistSubscriber = await getSubscribers
-    .json()
-    .then((data) => data.data[0]);
+  const isExistSubscriber = await getSubscribers.json();
 
-  if (!isExistSubscriber) {
+  if (!isExistSubscriber?.data?.length) {
     await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/subscribers", {
       method: "POST",
       body: JSON.stringify({
@@ -32,53 +28,6 @@ export async function POST(request: Request) {
         "Content-Type": "application/json",
       },
     });
-
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
-
-    const mailData = {
-      to: "minseob1826@gmail.com",
-      subject: "[일상백과] 임산부의 정확한 의미는?",
-      from: "minseob1826@gmail.com",
-      html: ` <div
-          style={{
-            background: "white",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifySelf: "center",
-            padding: "40px 20px",
-          }}
-        >
-          <h1
-            style={{
-              fontWeight: "800",
-            }}
-          >
-            오늘의 질문
-          </h1>
-          <div
-            style={{
-              backgroundColor: "#b8b6b6",
-              color: "#2A2A2A",
-              fontWeight: "bold",
-              marginTop: "20px",
-              padding: "20px",
-            }}
-          >
-            임산부의 정확한 뜻을 알고 계시나요?
-          </div>
-        </div>`,
-    };
-
-    transporter.sendMail(mailData);
   }
 
   return Response.json({});
