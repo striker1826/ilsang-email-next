ec86d6858566817e7bd403e43d8f447f5602ab3b
김민섭 <alstjq123579@daum.net>
2025-02-02 20:35:34 +0900

    Refactor: Footer의 이메일 주소 변경

--- components/templates/Footer/index.tsx
+++ components/templates/Footer/index.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="w-full py-[48px] text-[#cbd5e1] text-[12px] bg-[#1e293b] r-lg:w-full r-lg:flex r-lg:justify-center">
       <div className="px-[24px] ">
         <p>Copyright © 2025, 일상백과. All rights reserved.</p>
-        <p className="mt-[4px]">이메일: minseob1826@gmail.com</p>
+        <p className="mt-[4px]">이메일: ilsang.baekgwa@gmail.com</p>
       </div>
     </footer>
   );
