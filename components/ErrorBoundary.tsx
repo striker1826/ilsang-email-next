eb0e1aabb7001c959869454d3574f7f994d726ce
김민섭 <alstjq123579@daum.net>
2025-02-06 19:52:55 +0900

    fix: build error

--- components/ErrorBoundary.tsx
+++ components/ErrorBoundary.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Component, ErrorInfo, ReactNode } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 interface Props {
   children: ReactNode;
