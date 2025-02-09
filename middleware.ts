da4e2bae1d1dea81660105d0c1e1a55d7387eeb4
김민섭 <alstjq123579@daum.net>
2025-02-09 15:46:39 +0900

    hotfix

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,29 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
-  const url = req.nextUrl;
-  const pathname = url.pathname;
+// export function middleware(req: NextRequest) {
+//   const url = req.nextUrl;
+//   const pathname = url.pathname;
 
-  // API 요청, Next.js 내부 리소스 요청, 정적 파일 요청 제외
-  if (
-    pathname.startsWith("/api") || // API 요청 제외
-    pathname.startsWith("/_next") || // Next.js 내부 리소스 제외
-    pathname.startsWith("/static") // 정적 파일 요청 제외
-  ) {
-    return NextResponse.next();
-  }
+//   // API 요청, Next.js 내부 리소스 요청, 정적 파일 요청 제외
+//   if (
+//     pathname.startsWith("/api") || // API 요청 제외
+//     pathname.startsWith("/_next") || // Next.js 내부 리소스 제외
+//     pathname.startsWith("/static") // 정적 파일 요청 제외
+//   ) {
+//     return NextResponse.next();
+//   }
 
-  // www.이 없는 경우 www. 추가하여 리다이렉트
-  if (
-    !url.hostname.startsWith("www") &&
-    process.env.NODE_ENV === "production"
-  ) {
-    url.hostname = `www.${url.hostname}`;
-    return NextResponse.redirect(url);
-  }
+//   // www.이 없는 경우 www. 추가하여 리다이렉트
+//   if (!url.host.startsWith("www") && process.env.NODE_ENV === "development") {
+//     url.host = `www.${url.host}`;
+//     return NextResponse.redirect(url);
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-// 미들웨어 적용 경로 설정 (모든 페이지 요청에 적용)
-export const config = {
-  matcher: "/:path*",
-};
+// // 미들웨어 적용 경로 설정 (모든 페이지 요청에 적용)
+// export const config = {
+//   matcher: "/:path*",
+// };
