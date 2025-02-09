db1672c53960a7d5b793d30cfb4ff798cefc8ee4
김민섭 <alstjq123579@daum.net>
2025-02-09 15:52:41 +0900

    feat: www. 으로 리다이렉트 시키기

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "ilsang-baekgwa.com", // 변경할 도메인 (www. 없이 접속하는 경우)
+          },
+        ],
+        destination: "https://www.ilsang-baekgwa.com/:path*", // www.가 포함된 URL
+        permanent: true, // 301 리다이렉트 (영구 리다이렉트)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
